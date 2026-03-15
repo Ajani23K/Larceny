91b88dabdf444a14438a24098e864d6c507d9b57
Ajani King <ajaniking16@gmail.com>
2026-03-15 11:48:59 -0400

    added fps and player movement

--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -2,6 +2,8 @@ package main;
 
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
 
 import javax.swing.JPanel;
 
@@ -19,14 +21,24 @@ public class GamePanel extends JPanel implements Runnable{
 	final int ScreenWidth = tileSize * MaxScreenCol; //960 pixels
 	final int ScreenHeight = tileSize * MaxScreenRow; //768 pixels
 	
+	//FPS = frames per second
+	int FPS = 60;
+	
+	KeyHandler keyH = new KeyHandler(); //adding keyhandler so we can move character
 	Thread GameThread; //this will allow us to run mechanics indefinitely, such as creating 60 frames per second
 	
+	//set player default location
+	int playerX = 100;
+	int playerY = 100;
+	int playerSpeed = 4; 
 	//constructor to set panel size
 	public GamePanel() {
 		
 		this.setPreferredSize(new Dimension(ScreenWidth, ScreenHeight));
 		this.setBackground(Color.black);
 		this.setDoubleBuffered(true);
+		this.addKeyListener(keyH);
+		this.setFocusable(true);
 	}
 	
 	public void StartGameThread() {
@@ -36,9 +48,73 @@ public class GamePanel extends JPanel implements Runnable{
 	@Override
 	public void run() {
 		// implementation of core game loop
+		//as long as game thread exists this loop will run
+		
+		double drawInterval = 1000000000/FPS; //we draw the screen 0.01666 or 60 times a second
+		double nextDrawTime = System.nanoTime() + drawInterval; //declare the next draw time to be the next 60th of a second
+		
+		while(GameThread != null) {
+			
+			long currentTime = System.nanoTime();
+			
+			//purpose of loop
+			//update player information
+			update();
+			
+			//draw on screen with updated information
+			repaint(); //call paintComponent by calling repaint 
+			
+			try {
+				double remainingTime = nextDrawTime - System.nanoTime();
+				
+				//convert time into milliseconds
+				remainingTime = remainingTime/1000000;
+				
+				if(remainingTime < 0) {
+					remainingTime = 0;
+				}
+				//pause the game until time passes
+				Thread.sleep((long) remainingTime);
+				
+				nextDrawTime += drawInterval; 
+				
+				
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		// when player hits a key the input recorded and the player icon is updated/moved
 		
 	}
-	
+	public void update() {
+		//changing position of character based on user input
+		if(keyH.upPressed == true) {
+			playerY -= playerSpeed;
+		}
+		if(keyH.leftPressed == true) {
+			playerX -= playerSpeed;
+		}
+		if(keyH.downPressed == true) {
+			playerY += playerSpeed;
+		}
+		if(keyH.rightPressed == true) {
+			playerX += playerSpeed;
+		}
+	}
+	public void paintComponent(Graphics g) {
+		
+		super.paintComponent(g);
+		
+		Graphics2D g2 = (Graphics2D)g;
+		
+		g2.setColor(Color.white);
+		
+		g2.fillRect(playerX, playerY, tileSize, tileSize); //declaring location of the color rectangle
+		
+		g2.dispose(); //good practice to save memory
+	}
 	
 	
 }
